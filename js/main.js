68aea244763d56b8750f2c407a8f8ae71f3de1fb
Leila Amiri <lili.amiri@gmail.com>
2020-02-20 15:31:53 +1100

    Second Commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 
 $(function() {
-	$("#carousel-first").on('mousewheel', function() {
-		$("#carousel-first").slideUp('10000');
-		$("#carousel-last").slideDown('10000');
+	$('#carousel').on('mousewheel', function(e){
+     if(e.originalEvent.wheelDelta < 0) {
+        $('#carousel-first').slideUp('10000');
+		$('#carousel-last').slideDown('10000');
 		$('.dot2').css('backgroundColor', '#ff00cc');
 		$('.dot1').css('backgroundColor', 'blue');
-	});
+     } 
+
+     if(e.originalEvent.wheelDelta > 0) {
+        
+		$('#carousel-last').slideUp('10000');
+		$('#carousel-first').slideDown('10000');
+		$('.dot2').css('backgroundColor', 'blue');
+		$('.dot1').css('backgroundColor', '#ff00cc');
+     }
+
+ });
 
 	$('.dot1').on('click', function() {
 		$('.dot2').css('backgroundColor', 'blue');
